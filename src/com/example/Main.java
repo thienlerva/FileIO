d002879e35f12ee9c9a452a6452aeac447e1c70b
Thien <thienxuanle@yahoo.com>
2019-11-12 18:40:09 -0500

    run shell script

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -1,36 +1,59 @@
 package com.example;
 
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        File file = new File("/Users/thienle/Documents/FileIO/FileFolder/");
-        String[] fileList = file.list();
-        for (String name : fileList) {
-            System.out.println(name);
-        }
+//        File file = new File("/Users/thienle/Documents/FileIO/FileFolder/");
+//        String[] fileList = file.list();
+//        for (String name : fileList) {
+//            System.out.println(name);
+//        }
+        readBashScript();
 
-        filterFile();
+    }
+
+    public static void readBashScript() {
+        try {
+            Process proc = Runtime.getRuntime().exec("/Users/thienle/Documents/FileIO/myscript.sh"); //Whatever you want to execute
+            BufferedReader read = new BufferedReader(new InputStreamReader(
+                    proc.getInputStream()));
+            try {
+                proc.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+            while (read.ready()) {
+                System.out.println(read.readLine());
+            }
+        } catch (IOException e) {
+
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void filterFile() {
-        File file = new File("/Users/thienle/Documents/FileIO/FileFolder/");
-        String[] files = file.list(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                if (name.toLowerCase().endsWith(".txt")) {
-                    return true;
-                } else {
-                    return false;
+        try {
+            File file = new File("/Users/thienle/Documents/FileIO/FileFolde/");
+            String[] files = file.list(new FilenameFilter() {
+                @Override
+                public boolean accept(File dir, String name) {
+                    if (name.toLowerCase().endsWith(".txt")) {
+                        return true;
+                    } else {
+                        return false;
+                    }
                 }
-            }
-        });
+            });
 
-        for (String f : files) {
-            System.out.println(f);
+            for (String f : files) {
+                System.out.println(f);
+            }
+        } catch (Exception e) {
+
+            System.out.println(e.getMessage());
         }
     }
 }
